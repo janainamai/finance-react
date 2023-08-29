6ab204c00abea7ba32464539d8b6071e0b5e7673
janainamai <janainamai@hotmail.com>
2023-08-29 16:45:44 -0300

    Implementada tela de criação de usuário

--- src/components/pages/auth/CreateUser.jsx
+++ src/components/pages/auth/CreateUser.jsx
@@ -2,57 +2,74 @@ import SubmitButton from '../../form/SubmitButton.jsx'
 import { useState } from 'react'
 import Input from '../../form/Input.jsx'
 import styles from './styles/NewUser.module.css'
+import { useNavigate } from 'react-router-dom'
 
 function CreateUser() {
 
-    function createUser(e) {
+    const navigate = useNavigate()
+    const [userAccount, setUserAccount] = useState()
+
+    const submit = (e) => {
         e.preventDefault()
-        console.log('Login: ', login)
-        console.log('Email: ', email)
-        console.log('Senha: ', password)
-        console.log('Confirmação da senha: ', confirmPassword)
+
+        createUser(userAccount)
     }
 
-    const [login, setLogin] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const [confirmPassword, setConfirmPassword] = useState()
+    function handleChange(e) {
+        setUserAccount({ ...userAccount, [e.target.name]: e.target.value })
+    }
+
+    function createUser(user) {
+        const postOptions = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        }
+
+        fetch('http://localhost:8080/auth/register', postOptions)
+            .then(data => {
+                navigate('/', { state: { message: 'Sua conta foi criada com sucesso, você receberá um email de autorização, aguarde e seja bem vindo!', type: 'success' } })
+            })
+
+    }
 
     return (
         <div className={styles.new_user_container}>
             <h1>Cadastre-se</h1>
-            
-            <form onSubmit={createUser}>
-                <Input 
-                    type='text'    
+
+            <form onSubmit={submit}>
+                <Input
+                    type='text'
                     text='Login'
                     name='login'
                     placeholder='Digite o seu login'
-                    handleOnChange={setLogin}
+                    handleOnChange={handleChange}
                 />
-                 <Input 
-                    type='email'    
+                <Input
+                    type='email'
                     text='Email'
                     name='email'
                     placeholder='Digite o seu email'
-                    handleOnChange={setEmail}
+                    handleOnChange={handleChange}
                 />
-               <Input 
-                    type='password'    
+                <Input
+                    type='password'
                     text='Senha'
                     name='password'
                     placeholder='Digite a sua senha'
-                    handleOnChange={setPassword}
+                    handleOnChange={handleChange}
                 />
-                 <Input 
-                    type='password'    
+                <Input
+                    type='password'
                     text='Confirmação de senha'
                     name='confirmPassword'
                     placeholder='Digite a sua senha novamente'
-                    handleOnChange={setConfirmPassword}
+                    handleOnChange={handleChange}
                 />
 
-                <SubmitButton text="Cadastrar"/>
+                <SubmitButton text="Cadastrar" />
             </form>
         </div>
     )
--- src/components/pages/welcome/WelcomeHome.jsx
+++ src/components/pages/welcome/WelcomeHome.jsx
@@ -1,12 +1,17 @@
 import styles from './styles/Welcome.module.css'
 import savings from '../../../images/savings.svg'
 import LinkButton from '../../form/LinkButton.jsx';
+import { useLocation } from 'react-router-dom';
+import Message from '../../layout/Message';
 
 function WelcomeHome() {
+
+    const location = useLocation()
 
     return (
 
         <section className={styles.home_container}>
+            {location?.state?.message && <Message message={location.state.message} type={location.state.type} />}
 
             <h1>Bem-vindo ao <span>Finance</span></h1>
             <p>Faça a gestão da sua renda de forma sustentável!</p>
